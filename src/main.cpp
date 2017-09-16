41d43e51ef629a4bdb5bca4cc13883bff4608a3e
Jacob Moen <jacmoe.dk@gmail.com>
2017-09-16 22:57:11 +0200

    We are moving

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include "libtcod/libtcod.hpp"
 
 int main() {
+    int playerx {40};
+    int playery {25};
     TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
     while ( !TCODConsole::isWindowClosed() ) {
         TCOD_key_t key;
         TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
+
+        switch(key.vk) {
+            case TCODK_UP : playery--;
+            break;
+            case TCODK_DOWN : playery++;
+            break;
+            case TCODK_LEFT : playerx--;
+            break;
+            case TCODK_RIGHT : playerx++;
+            break;
+            default:
+            break;
+        }
+
         TCODConsole::root->clear();
-        TCODConsole::root->putChar(40,25,'@');
+
+        TCODConsole::root->putChar(playerx, playery,'@');
+
         TCODConsole::flush();
     }
     return 0;
